58a82c5adb4fdf911fef278d8996947aa5f1b699
pien <minecraftinmybloodstream@gmail.com>
2022-04-24 19:17:00 -0400

    Add project files.

---
+++ リスカ/リスカ.cpp
@@ -0,0 +1,49 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <psapi.h>
+#include <tlhelp32.h>
+#include <conio.h>
+#include <shlobj_core.h>
+int main()
+{
+	DWORD processList[1024];
+	CHAR dllPath[MAX_PATH];
+	GetCurrentDirectoryA(MAX_PATH, dllPath);
+	strcat_s(dllPath, MAX_PATH, "\\pien.dll");
+
+	printf("Waiting for game...\n");
+
+	while (true)
+	{
+		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		PROCESSENTRY32 info;
+		info.dwSize = sizeof(PROCESSENTRY32);
+		Process32First(snapshot, &info);
+		do
+		{
+			HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, info.th32ProcessID);
+			if (handle != NULL)
+			{
+				char name[MAX_PATH];
+				GetModuleFileNameExA(handle, NULL, name, sizeof(name));
+				if (strstr(name, "ElyonGameExecutor.exe"))
+				{
+					printf("Injecting...\n");
+					LPVOID pDllPath = VirtualAllocEx(handle, NULL, strlen(dllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
+					WriteProcessMemory(handle, pDllPath, (LPVOID)dllPath, strlen(dllPath) + 1, 0);
+					HANDLE thread = CreateRemoteThread(handle, NULL, NULL, (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA"), pDllPath, NULL, NULL);
+					if (thread != NULL)
+					{
+						WaitForSingleObject(thread, INFINITE);
+						VirtualFreeEx(handle, pDllPath, strlen(dllPath) + 1, MEM_RELEASE);
+						printf("Xigncode bypassed. Press any key to exit.\n");
+						_getch();
+						return 0;
+					}
+				}
+				CloseHandle(handle);
+			}
+		} while (Process32Next(snapshot, &info));
+		CloseHandle(snapshot);
+	}
+}
---
+++ 自傷行為/dllmain.cpp
@@ -0,0 +1,77 @@
+﻿#include <Windows.h>
+#include <MinHook.h>
+#pragma comment(lib, "libMinHook.lib")
+
+typedef int(WINAPI* CREATEPROCESSW)
+(
+	LPCWSTR,
+	LPWSTR,
+	LPSECURITY_ATTRIBUTES,
+	LPSECURITY_ATTRIBUTES,
+	BOOL,
+	DWORD,
+	LPVOID,
+	LPCWSTR,
+	LPSTARTUPINFOW,
+	LPPROCESS_INFORMATION
+);
+
+CREATEPROCESSW OriginalCreateProcessW = nullptr;
+
+BOOL WINAPI HookCreateProcessW(LPCWSTR lpApplicationName,
+	LPWSTR lpCommandLine,
+	LPSECURITY_ATTRIBUTES lpProcessAttributes,
+	LPSECURITY_ATTRIBUTES lpThreadAttributes,
+	BOOL inheritHandles,
+	DWORD creationFlags,
+	LPVOID lpEnviornment,
+	LPCWSTR currentDirectory,
+	LPSTARTUPINFOW startupInfo,
+	LPPROCESS_INFORMATION processInformation)
+{
+	wchar_t commandline[500];
+	wcscpy_s(commandline, lpCommandLine);
+	wcscat_s(commandline, L" -noxigncode");
+	return OriginalCreateProcessW
+	(
+		lpApplicationName,
+		commandline,
+		lpProcessAttributes,
+		lpThreadAttributes,
+		inheritHandles,
+		creationFlags,
+		lpEnviornment,
+		currentDirectory,
+		startupInfo,
+		processInformation
+	);
+}
+
+DWORD WINAPI Init(LPVOID param)
+{
+	if (MH_Initialize() != MH_OK)
+	{
+		FreeLibraryAndExitThread((HMODULE)param, 0);
+		return 0;
+	}
+	if (MH_CreateHook(&CreateProcessW, &HookCreateProcessW, reinterpret_cast<LPVOID*>(&OriginalCreateProcessW)) != MH_OK)
+	{
+		FreeLibraryAndExitThread((HMODULE)param, 0);
+		return 0;
+	}
+	if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK)
+	{
+		FreeLibraryAndExitThread((HMODULE)param, 0);
+		return 0;
+	}
+	return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		DisableThreadLibraryCalls(instance);
+		CreateThread(NULL, NULL, Init, instance, NULL, NULL);
+	}
+	return TRUE;
+}
